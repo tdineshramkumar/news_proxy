b79682039e7a76f1f9bb95bff4226493755a62be
Dinesh Ram Kumar <tdineshramkumar@gmail.com>
2019-01-25 10:13:38 +0530

    Modified measure for parallel requests

--- measure/main.go
+++ measure/main.go
@@ -3,29 +3,45 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 )
 
 func main() {
-	var port, numTrials int
-	flag.IntVar(&port, "port", 9999, "webserver port number")
-	flag.IntVar(&numTrials, "trials", 1, "number of requests to send")
+	var port, numTrials, numRoutines int
+	var serverDomain string
+	flag.IntVar(&port, "port", 9999, "port number of webserver running in localhost.")
+	flag.IntVar(&numTrials, "trials", 1, "number of requests to send per routine.")
+	flag.IntVar(&numRoutines, "routines", 1, "number of concurrent routines to send request.")
+	flag.StringVar(&serverDomain, "server", "localhost", "domain name of the server to test.")
 	flag.Parse()
 
-	URI := fmt.Sprintf("http://localhost:%d/", port)
-	for i := 0; i < numTrials; i++ {
-		start := time.Now()
-		// Send the request to the server
-		_, err := http.Get(URI)
-		responseTime := time.Now().Sub(start)
-		outputString := fmt.Sprintf("%f", responseTime.Seconds())
-		if err != nil {
-			// some error occurred.
-			fmt.Fprintln(os.Stderr, outputString)
-			continue
-		}
-		fmt.Fprintln(os.Stdout, outputString)
+	URL := fmt.Sprintf("http://%s:%d/", serverDomain, port)
+	var wg sync.WaitGroup
+	for routine := 0; routine < numRoutines; routine++ {
+		wg.Add(1)
+		go func() {
+
+			for trial := 0; trial < numTrials; trial++ {
+				start := time.Now()
+				// Send the request to the server
+				resp, err := http.Get(URL)
+				io.Copy(ioutil.Discard, resp.Body)
+				resp.Body.Close()
+				duration := time.Now().Sub(start)
+				responseTime := fmt.Sprintf("%f", duration.Seconds())
+				if err != nil {
+					fmt.Fprintln(os.Stderr, "[", responseTime, "]", err)
+					continue
+				}
+				fmt.Fprintln(os.Stdout, responseTime)
+			}
+			wg.Done()
+		}()
 	}
+	wg.Wait()
 }
